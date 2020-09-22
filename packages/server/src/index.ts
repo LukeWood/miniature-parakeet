76f9e65bcb50104bd25d675f2501926bd7aa37f0
Luke Wood <lukewoodcs@gmail.com>
2020-09-22 17:06:37 -0600

    Implement bare minimum colyseus server + react frontend

---
+++ packages/server/src/index.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+
+function findPort(): number {
+  return 8001;
+}
+
+const app = express();
+app.use(express.json());
+
+const server = new Server({
+  server: createServer(app),
+  express: app
+})
+
+const PORT = findPort();
+server.listen(PORT);
+console.log(`Listening on ws://localhost:${PORT}`);
